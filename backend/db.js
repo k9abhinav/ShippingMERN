6e1e6d6d469ab3b250d6bbb35738f09c947beeb5
k9abhinav <k9abhinava@gmail.com>
2023-11-24 10:48:39 +0530

    ENV

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,8 @@
+require()
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("mongodb+srv://abhinava03kks:abhinavamongodb@cluster0.tzfaz9x.mongodb.net/?retryWrites=true&w=majority").then((response) => {
+    .connect(process.env.SECRETKEY).then((response) => {
     console.log("Connected to database")
 })
     .catch((error)=>{
